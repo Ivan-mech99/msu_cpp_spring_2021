6748b83761cdafd88abf497302d59f0091991f41
Ivan-mech99 <58103054+Ivan-mech99@users.noreply.github.com>
2021-03-22 07:55:53 +0300

    Add files via upload

---
+++ 01/class.h
@@ -0,0 +1,13 @@
+class Allocator {
+  size_t mem_amount = 0;
+  size_t free_mem_amount = 0;
+  char *mem_begin = nullptr;
+
+ public:
+  Allocator();
+  ~Allocator();
+  void makeAllocator(size_t maxSize);
+  char *alloc(size_t size);
+  void reset();
+  void show_info();
+};
