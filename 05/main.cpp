8ad73ad07b26db6e54e7ee78c470c8a4776cb9c9
Ivan-mech99 <58103054+Ivan-mech99@users.noreply.github.com>
2021-05-14 20:43:59 +0300

    Add files via upload

---
+++ 05/main.cpp
@@ -0,0 +1,176 @@
+#include <iostream>
+#include <sstream>
+#include <cassert>
+#include "my_serializer.h"
+
+void Default_test() {
+  Data x{1, true, 2};
+  std::stringstream stream;
+  Serializer serializer(stream);
+  const Error errs = serializer.save(x);
+  assert(errs == Error::NoError);
+  string res = "1 true 2 ";
+  assert(res == stream.str());
+  Data y{0, false, 0};
+  Deserializer deserializer(stream);
+  const Error err = deserializer.load(y);
+  assert(err == Error::NoError);
+  assert(x.a == y.a);
+  assert(x.b == y.b);
+  assert(x.c == y.c);
+}
+
+void Only_bool_test() {
+  bool3 x{false, true, true};
+  std::stringstream stream;
+  Serializer serializer(stream);
+  const Error errs = serializer.save(x);
+  assert(errs == Error::NoError);
+  string res = "false true true ";
+  assert(res == stream.str());
+  Data y{0, false, 0};
+  Deserializer deserializer(stream);
+  const Error err = deserializer.load(y);
+  assert(err == Error::CorruptedArchive);
+  assert(0 == y.a);
+  assert(false == y.b);
+  assert(0 == y.c);
+}
+
+void Incorrect_fields_num_test() {
+  cout << "Serialize_2_to_3" << endl;
+  num_bool x{111, true};
+  std::stringstream stream;
+  Serializer serializer(stream);
+  const Error errs = serializer.save(x);
+  assert(errs == Error::NoError);
+  string res1 = "111 true ";
+  assert(res1 == stream.str());
+  Data y{1, true, 2};
+  Deserializer deserializer(stream);
+  const Error err = deserializer.load(y);
+  assert(err == Error::CorruptedArchive);
+  assert(y.a == x.a);
+  assert(y.b == x.b);
+  assert(y.c == 2);
+  cout << "Serialize_4_to_3" << endl;
+  num_bool_num_num z{1, true, 78, 55};
+  std::stringstream stream1;
+  Serializer serializer1(stream1);
+  const Error errs1 = serializer1.save(z);
+  assert(errs1 == Error::NoError);
+  string res2 = "1 true 78 55 ";
+  assert(res2 == stream1.str());
+  Deserializer deserializer1(stream1);
+  const Error err2 = deserializer1.load(y);
+  assert(y.a == z.a);
+  assert(y.b == z.b);
+  assert(y.c == z.c);
+  assert(err2 == Error::CorruptedArchive);
+}
+
+void One_field_structures_test() {
+  cout << "One-field bool test!" << endl;
+  just_bool x{true};
+  std::stringstream stream;
+  Serializer serializer(stream);
+  const Error errs = serializer.save(x);
+  assert(errs == Error::NoError);
+  string res1 = "true ";
+  assert(res1 == stream.str());
+  just_bool y{false};
+  Deserializer deserializer(stream);
+  const Error err = deserializer.load(y);
+  assert(err == Error::NoError);
+  assert(x.a == y.a);
+  cout << "One-field uint_64t test!" << endl;
+  just_num x1{1600};
+  std::stringstream stream1;
+  Serializer serializer1(stream1);
+  const Error errs1 = serializer1.save(x1);
+  assert(errs1 == Error::NoError);
+  string res2 = "1600 ";
+  assert(res2 == stream1.str());
+  just_num y1{243};
+  Deserializer deserializer1(stream1);
+  const Error err1 = deserializer1.load(y1);
+  assert(err1 == Error::NoError);
+  assert(x1.a == y1.a);
+}
+
+void Incorrect_numerical_tokens() {
+  cout << "Biggest numerical token test" << endl;
+  uint64_t temp_hold = 18446744073709551615UL;
+  just_num x1{temp_hold};
+  std::stringstream stream1;
+  Serializer serializer1(stream1);
+  const Error errs = serializer1.save(x1);
+  assert(errs == Error::NoError);
+  string res1 = "18446744073709551615 ";
+  assert(res1 == stream1.str());
+  just_num y1{243};
+  Deserializer deserializer1(stream1);
+  const Error err1 = deserializer1.load(y1);
+  assert(err1 == Error::NoError);
+  assert(x1.a == y1.a);
+  cout << "Token out of limitt" << endl;
+  string token0 = "19446744073709551615 ";
+  std::stringstream stream2;
+  stream2 << token0;
+  just_num y2{543};
+  Deserializer deserializer2(stream2);
+  const Error err2 = deserializer2.load(y2);
+  assert(err2 == Error::CorruptedArchive);
+  assert(543 == y2.a);
+  cout << "Not completely numerical token" << endl;
+  std::stringstream stream3;
+  string token = "1z446744073709551615 ";
+  stream3 << token;
+  just_num y3{543};
+  Deserializer deserializer3(stream3);
+  const Error err3 = deserializer3.load(y3);
+  assert(err3 == Error::CorruptedArchive);
+  assert(543 == y3.a);
+}
+
+void Special_cases_01false_01_test() {
+  num_num_bool x{0, 1, false};
+  std::stringstream stream;
+  Serializer serializer(stream);
+  const Error errs = serializer.save(x);
+  assert(errs == Error::NoError);
+  string res = "0 1 false ";
+  assert(res == stream.str());
+  bool3 y{false, false, true};
+  Deserializer deserializer(stream);
+  const Error err = deserializer.load(y);
+  assert(err == Error::CorruptedArchive);
+  assert(y.a == false);
+  assert(y.b == false);
+  assert(y.c == true);
+  num_num x1{0, 1};
+  std::stringstream stream1;
+  Serializer serializer1(stream1);
+  const Error errs1 = serializer1.save(x1);
+  assert(errs1 == Error::NoError);
+  string res1 = "0 1 ";
+  assert(res1 == stream1.str());
+  num_num y1{546, 756};
+  Deserializer deserializer1(stream1);
+  const Error err1 = deserializer1.load(y1);
+  assert(err1 == Error::NoError);
+  assert(y1.a == 0);
+  assert(y1.b == 1);
+}
+
+int main() {
+  Default_test();
+  Only_bool_test();
+  Incorrect_fields_num_test();
+  One_field_structures_test();
+  Incorrect_numerical_tokens();
+  Special_cases_01false_01_test();
+  cout << "Success!" << endl;
+  return 0;
+}
+
---
+++ 05/my_serializer.h
@@ -0,0 +1,181 @@
+#ifndef H_MY_SERIALIZER
+#define H_MY_SERIALIZER
+#pragma once
+#include <iostream>
+#include <string>
+using std::string;
+using std::ostream;
+using std::istream;
+using std::forward;
+using std::cout;
+using std::endl;
+
+enum class Error { NoError, CorruptedArchive };
+
+class Serializer {
+  ostream &out_;
+  static constexpr char Separator = ' ';
+
+ public:
+  explicit Serializer(ostream &out) : out_(out) {}
+
+  template <class T>
+  Error save(T &object) {
+    return object.serialize(*this);
+  }
+
+  template <class... ArgsT>
+
+  Error operator()(ArgsT... args) {
+    return process(args...);
+  }
+
+ private:
+  template <class T, class... ArgsT>
+  Error process(T &&value, ArgsT &&... args) {
+    inner_save(value);
+    return process(args...);
+  }
+
+  template <class T>
+  Error process(T &&value) {
+    inner_save(value);
+    return Error::NoError;
+  }
+
+  void inner_save(bool &value);
+  void inner_save(uint64_t &value);
+};
+
+class Deserializer {
+  istream &in_;
+
+ public:
+  explicit Deserializer(istream &in) : in_(in) {}
+
+  template <class T>
+  Error load(T &object) {
+    return object.serialize(*this);
+  }
+
+  template <class... ArgsT>
+  Error operator()(ArgsT &&... args) {
+    return process(args...);
+  }
+
+ private:
+  template <class T, class... ArgsT>
+  Error process(T &&value, ArgsT &&... args) {
+    Error err;
+    err = inner_load(value);
+    if (err == Error::CorruptedArchive) {
+      return Error::CorruptedArchive;
+    } else {
+      return process(args...);
+    }
+  }
+
+  Error check_extra_fields();
+
+  template <class T>
+  Error process(T &&value) {
+    Error err;
+    err = inner_load(value);
+    if (err == Error::CorruptedArchive) {
+      return Error::CorruptedArchive;
+    } else {
+      return check_extra_fields();
+    }
+  }
+
+  Error inner_load(bool &value);
+  Error inner_load(uint64_t &value);
+};
+
+struct Data {
+  uint64_t a;
+  bool b;
+  uint64_t c;
+
+  template <class Serializer>
+  Error serialize(Serializer &serializer) {
+    return serializer(a, b, c);
+  }
+};
+
+struct num_bool {
+  uint64_t a;
+  bool b;
+
+  template <class Serializer>
+  Error serialize(Serializer &serializer) {
+    return serializer(a, b);
+  }
+};
+
+struct bool_num {
+  bool a;
+  uint64_t b;
+  template <class Serializer>
+  Error serialize(Serializer &serializer) {
+    return serializer(a, b);
+  }
+};
+
+struct num_bool_num_num {
+  uint64_t a;
+  bool b;
+  uint64_t c;
+  uint64_t d;
+  template <class Serializer>
+  Error serialize(Serializer &serializer) {
+    return serializer(a, b, c, d);
+  }
+};
+
+struct just_bool {
+  bool a;
+  template <class Serializer>
+  Error serialize(Serializer &serializer) {
+    return serializer(a);
+  }
+};
+
+struct just_num {
+  uint64_t a;
+  template <class Serializer>
+  Error serialize(Serializer &serializer) {
+    return serializer(a);
+  }
+};
+
+struct bool3 {
+  bool a;
+  bool b;
+  bool c;
+  template <class Serializer>
+  Error serialize(Serializer &serializer) {
+    return serializer(a, b, c);
+  }
+};
+
+struct num_num {
+  uint64_t a;
+  uint64_t b;
+  template <class Serializer>
+  Error serialize(Serializer &serializer) {
+    return serializer(a, b);
+  }
+};
+
+struct num_num_bool {
+  uint64_t a;
+  uint64_t b;
+  bool c;
+  template <class Serializer>
+  Error serialize(Serializer &serializer) {
+    return serializer(a, b, c);
+  }
+};
+
+#endif
